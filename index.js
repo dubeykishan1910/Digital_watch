dddad98a1573f9d3ead464cc8e333b5e66c203ef
dubeykishan1910 <dubeykishan1910@gmail.com>
2024-01-03 22:07:21 +0530

    counter

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
-const hourE1 = document.getElementById("hours");
-const minuteE1 = document.getElementById("minutes");
-const secondE1 = document.getElementById("seconds");
-const ampmE1 = document.getElementById("ampm");
+// const hourE1 = document.getElementById("hours");
+// const minuteE1 = document.getElementById("minutes");
+// const secondE1 = document.getElementById("seconds");
+// const ampmE1 = document.getElementById("ampm");
 
 
-function updateClock(){
-let h= new Date().getHours();
-let m= new Date().getMinutes();
-let s= new Date().getSeconds();
-let ampm = "AM";
+// function updateClock(){
+// let h= new Date().getHours();
+// let m= new Date().getMinutes();
+// let s= new Date().getSeconds();
+// let ampm = "AM";
 
-if (h>12){
-    h=h-12;
-    ampm = "PM";
-}
+// if (h>12){
+//     h=h-12;
+//     ampm = "PM";
+// }
 
-h=h<10 ? "0"+ h : h;
-m=m<10 ? "0"+ m : m;
-s=s<10 ? "0"+ s : s;
+// h=h<10 ? "0"+ h : h;
+// m=m<10 ? "0"+ m : m;
+// s=s<10 ? "0"+ s : s;
 
-hourE1.innerText = h;
-minuteE1.innerText = m;
-secondE1.innerText = s;
-ampmE1.innerText = ampm;
+// hourE1.innerText = h;
+// minuteE1.innerText = m;
+// secondE1.innerText = s;
+// ampmE1.innerText = ampm;
 
-setTimeout(()=>{
-    updateClock()}
-    ,1000)
+// setTimeout(()=>{
+//     updateClock()}
+//     ,1000)
 
-}
+// }
 
 
-updateClock()
+// updateClock()
 
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    function counter(id ,start,end, duration){
+        let obj = document.getElementById(id),
+        current= start,
+        range= end-start,
+        increment = end > start ? 1 :-1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(()=>{
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        },step);
+    }
+    counter("count1",0,1287,100)
+
+})
